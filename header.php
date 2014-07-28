2801c10a16b034b48368a7bb8ff53c50edfb45e4
Alex <alex.averill2013@gmail.com>
2014-07-27 23:52:00 -0500

    Fixing password issues

--- header.php
+++ header.php
@@ -1,5 +1,9 @@
 <?php
 include('database.php');
+if (version_compare(phpversion(), '5.5.0', '<')) {
+    //If the php Version is not supporting password hash, lets include it
+    require('source/lib/password.php');
+}
 try{
     $dbh= new PDO('mysql:host='.$data_host.';dbname='.$name_database,$data_username,$data_password);
 }catch(PDOException $e){
