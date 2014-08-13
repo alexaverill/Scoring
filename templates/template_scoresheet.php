831e87ef781a4a17b8ffe271ddc0af1bcdf5640c
Alex <alex.averill2013@gmail.com>
2014-08-13 13:48:29 -0500

    more helpful text

--- templates/template_scoresheet.php
+++ templates/template_scoresheet.php
@@ -490,7 +490,7 @@ $teamsToRank = $settings->teamsToRank($division);
 			    warningLocation = "connection";
 			    if (!connectionStatus) {
 				document.getElementById(warningLocation).className="alert alert-warning";
-				document.getElementById(warningLocation).innerHTML = "Currently operating without a connection";
+				document.getElementById(warningLocation).innerHTML = "Currently operating without a connection, data will be saved to the server once connection is restored";
 				connection = false;
 				return false;
 			    }else{
