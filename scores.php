ea5eab3db7670d2315b7b7cf32bed7566f94e381
Alex <alex.averill2013@gmail.com>
2014-08-13 16:13:34 -0500

    Fixing a permission issue

--- scores.php
+++ scores.php
@@ -1,17 +1,18 @@
 <?php
 include('header.php');
-
+$user = new user;
 $display = new display;
+$permissions = $user->return_permissions($_SESSION['id']);
 if(isset($_GET['event'])){
     $eventId = $_GET['event'];
 }else{
     
      
 }
-if($eventId == 0 ){
-   
-}else{
+if(in_array($eventId,$permissions) ){
     $display->score_sheet($eventId);
+}else{
+    echo 'You do not have permission to view that event.';
 }
 
 ?>
