c9ecbc82b628945a3be242cac4cd7733d5ee2410
Alex <alex.averill2013@gmail.com>
2014-08-19 12:09:48 -0500

    Fixing login

--- login.php
+++ login.php
@@ -5,6 +5,26 @@ if($_POST['login']){
 	$user->login($_POST['user'],$_POST['pass']);
 }
 ?>
+<html>
+	<head>
+		<script type="text/javascript" src="source/jquery-2.1.1.min.js"></script>
+		<script src="source/jquery.jeditable.js" type="text/javascript"></script>
+				<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="source/bootstrap/css/bootstrap.css">
+		<!-- Extra Themes -->
+		<link rel="stylesheet" href="source/parts.css">
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="source/bootstrap/js/bootstrap.min.js"></script>
+		<script src="source/editing.js"></script>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<title>Score Sheet</title>
+		<script type="text/javascript" src="source/jquery-2.1.1.min.js"></script>
+		
+	</head>
+	
+	<body>
+		
+	<div class="class="container-fluid">
 <h1>Login</h1>
 		<div class="user_info">
 			<form method="POST" action="">
