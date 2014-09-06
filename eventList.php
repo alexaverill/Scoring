a67c7d50d92244a508224a2d233f08b6b30de0d6
Alex Averill <alex.averill2013@gmail.com>
2014-09-06 15:35:49 -0500

    fixing template

--- eventList.php
+++ eventList.php
@@ -4,5 +4,5 @@ $events = new events;
 $eventsB= $events->return_events(1,'B');
 $eventsC= $events->return_events(1,'C');
 
-echo $twig->render('adminUsers.html',array('eventB'=>$eventsB,'eventC'=>$eventsC));
+echo $twig->render('eventList.html',array('eventB'=>$eventsB,'eventC'=>$eventsC));
 ?>
