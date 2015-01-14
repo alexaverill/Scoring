6a32311b66c1f090519f2dca1da17e44b746a936
Alex Averill <alex.averill2013@gmail.com>
2015-01-13 23:38:24 -0600

    Changing things for admins

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ if($_SESSION['type']==2){
     $display->list_events(2,'B');
     $display->list_events(2,'C');
 }
-if($_SESSION['type']==3 && in_array(999,$permissions)){
+if(($_SESSION['type']==3 && in_array(999,$permissions))){
    echo '<h3>Event Supervisor Select Your Event:</h3>';
 $display = new display;
 $display->list_events(1,'B');
@@ -55,23 +55,13 @@ if($_SESSION['type']==3 && !in_array(999,$permissions)){
         echo '</table>';
 }
 if($_SESSION['type']==1){
+	   echo '<h2>Enter Scores</h2>';
+$display = new display;
+$display->list_events(1,'B');
+$display->list_events(1,'C');
+    echo'<h2>Verify Event Scores</h2>';
     $display = new display;
-$events= new events;
-$stats = new stats;
-//check forms
-
-
-echo '
-<h3><a href="admin_events.php">Add and Change Active Events</a></h3>
-<h3><a href="admin_teams.php">Add Teams</a></h3>
-<h3><a href="admin_users.php">Users and Permissions</a></h3>
-<h3><a href="final.php?division=B">Confirm B Rankings</a></h3>
-<h3><a href="final.php?division=C">Confirm C Rankings</a></h3>
-<!--<h3>Current Competition</h3>
-<table class="table table-striped table-bordered table-condensed table-hover">
-    <tr><th><a href="admin_comp.php">Event Scoring Completed</a></th><th>Event Scoring Started</th><th>Event Scoring Unstarted</th></tr>
-    <tr><td><?php echo $stats->numberCompleted();?>/<?php echo $events->number_events(); ?></td><td>Statistics Here</td><td>Statistics Here</td></tr>
-</table>-->
-';
+    $display->list_events(2,'B');
+    $display->list_events(2,'C');
 }
 ?>
